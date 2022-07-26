43f6eb66bda975f219a3322fcb041c7df721f8ef
BZIEO <100628007+BZIEO@users.noreply.github.com>
2022-07-26 16:55:41 -0500

    Created basic math operator functions

---
+++ script.js
@@ -0,0 +1,21 @@
+//Basic math operators 
+function add(x,y) {
+    let res = x + y;
+    return res
+}
+
+function subtract(x,y) {
+    let res = x-y;
+    return res
+}
+
+function multiply(x,y) {
+    let res = x*y;
+    return res
+}
+
+function divide(x,y) {
+    let res = x/y;
+    return res
+}
+
